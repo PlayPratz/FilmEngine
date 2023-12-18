c74b487934ea4a455515f592bdf8e89557376bae
PlayPratz <playpratz@gmail.com>
2023-12-18 14:08:59 +0530

    Extremely basic landing page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,15 @@
 </script>
 
 <template>
-  <main>
-    
-  </main>
+  <div class="fullview">
+    <h1 class="title">
+      THE<br />
+      FILM<br />
+      ENGINE<br />
+    </h1>
+    <br /> <br /> <br />
+    <h5 class="bolder">
+      COMING SOON
+    </h5>
+  </div>
 </template>
