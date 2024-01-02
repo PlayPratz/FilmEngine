241cd7bcd2e50e0ea7f5a8a67a6da5a531ad82a1
PlayPratz <playpratz@gmail.com>
2024-01-02 14:44:32 +0530

    Switched to Hash routing, resized pictures

--- src/router.ts
+++ src/router.ts
@@ -1,20 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory,  } from 'vue-router'
 import HomeView from './views/HomeView.vue'
 import MeetUsView from './views/MeetUsView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
       component: HomeView,
-      children: [
-        {
-          path: ':pathMatch(.*)*',
-          redirect: '/'
-        }
-      ]
     },
     {
       path: '/meet-us',
